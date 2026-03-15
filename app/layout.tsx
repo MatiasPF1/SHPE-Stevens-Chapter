4ce9474ffab50fedf3358d778c62814d56efdc1c
Matias Freire <matiaspfreire@gmail.com>
2026-03-15 18:24:29 -0400

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,13 @@ import Navbar from "@/app/Navbar";
 import Footer from "@/app/Footer";
 import "./globals.css";
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://shpestevens.org";
+const siteUrl =
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : undefined) ||
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined) ||
+  "https://shpe-stevens-chapter.vercel.app";
 
 const montserrat = Montserrat({ 
   subsets: ["latin"],
